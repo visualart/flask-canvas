39ae36775cac395a064cecd5b4c3866373a3aa9e
Joar Wandborg <git@wandborg.com>
2013-02-15 10:46:15 +0100

    Added LICENSE

--- flask_canvas/__init__.py
+++ flask_canvas/__init__.py
@@ -1,3 +1,19 @@
+# flask-canvas - Flask blueprint for integration with Facebook Canvas
+# Copyright (C) 2012  Joar Wandborg, Visual Art AB <http://visualart.se>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import hmac
 import facebook
 from hashlib import sha256
